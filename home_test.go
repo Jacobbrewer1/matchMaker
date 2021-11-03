19071c79668492d5ad0dfb39b36ac858b9dc2214
Jacob Brewer <JacobBrewer2000@outlook.com>
2021-11-03 17:44:42 +0000

    Adding unit tests for addPlayers

--- home_test.go
+++ home_test.go
@@ -70,6 +70,73 @@ func setupSingleGenderPlayers(gender bool) []playerType {
 	return testPlayers
 }
 
+func setupErrorPlayers() []playerType {
+	playerCount := 0
+	var testPlayers []playerType
+	for {
+		var tempPlayer playerType
+
+		tempPlayer.fname = "Test"
+		tempPlayer.lname = strconv.Itoa(playerCount)
+
+		testPlayers = append(testPlayers, tempPlayer)
+		playerCount++
+		if playerCount > 9 {
+			break
+		}
+	}
+	return testPlayers
+}
+
+func setupGenderStrings(playerCount int, blank bool) []string {
+	var genders []string
+	localCount := 0
+	for {
+		if blank && localCount < playerCount {
+			genders = append(genders, "")
+			localCount++
+			continue
+		}
+		if localCount%2 == 0 {
+			genders = append(genders, "Male")
+		} else {
+			genders = append(genders, "Female")
+		}
+
+		localCount++
+		if localCount > playerCount {
+			break
+		}
+	}
+	return genders
+}
+
+func Test_addPlayer(t *testing.T) {
+	tests := []struct {
+		name           string
+		players        []playerType
+		genders        []string
+		expectedResult bool
+	}{
+		{"Even Count", setupPlayersArray(), setupGenderStrings(10, false), true},
+		{"All male", setupSingleGenderPlayers(true), setupGenderStrings(10, false), true},
+		{"All female", setupSingleGenderPlayers(false), setupGenderStrings(10, false), true},
+		{"Fail players", setupPlayersArray(), setupGenderStrings(10, true), false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for personId, person := range tt.players {
+				got := addPlayer(person.fname, person.lname, tt.genders[personId])
+				if got != tt.expectedResult {
+					t.Errorf("getPlayerCount() = %v, expected %v", got, tt.expectedResult)
+				}
+			}
+		})
+	}
+
+}
+
 func Test_getPlayerCount(t *testing.T) {
 	tests := []struct {
 		name                string
